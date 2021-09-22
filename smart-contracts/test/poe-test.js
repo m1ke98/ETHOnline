667fffc20a152cd4ee39cea3073aeb33500f33e5
Michael D'Argenio <mdargenio14@gmail.com>
2021-09-21 22:56:21 -0400

    more robust tests

--- smart-contracts/test/poe-test.js
+++ smart-contracts/test/poe-test.js
@@ -1,21 +1,51 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("PoeNft", function () {
-    it("Should show the owner has a single token", async function () {
-        const [owner] = await ethers.getSigners();
+describe("Token contract", function () {
+    let PoeNft;
+    let poeNft;
+    let owner;
+    let addr1;
 
-        const PoeNft = await ethers.getContractFactory("PoeNft");
-        const poeNft = await PoeNft.deploy();
-        await poeNft.deployed();
+    beforeEach(async function () {
+        PoeNft = await ethers.getContractFactory("PoeNft");
+        [owner, addr1] = await ethers.getSigners();
 
-        expect(await poeNft.balanceOf(owner.address)).to.equal(0);
+        poeNft = await PoeNft.deploy();
+    });
 
-        const mintPoe = await poeNft.mintToken(owner.address, "https://your-metadata-api.herokuapp.com/api/token/");
+    describe("Mint", function () {
+        it("Should show the owner doesn't have any tokens", async function () {
+            expect(await poeNft.balanceOf(owner.address)).to.equal(0);
+        });
 
-        // wait until the transaction is mined
-        await mintPoe.wait();
+        it("Should show the owner has a single token", async function () {
+            const mintPoe = await poeNft.mintToken(owner.address, "https://your-metadata-api.herokuapp.com/api/token/");
+            await mintPoe.wait();
+    
+            expect(await poeNft.balanceOf(owner.address)).to.equal(1);
+        });
+    });
+    
+    describe("Transfer", function () {
+        it("Should transfer tokens", async function () {
+            const mintPoe = await poeNft.mintToken(owner.address, "https://your-metadata-api.herokuapp.com/api/token/");
+            await mintPoe.wait();
 
-        expect(await poeNft.balanceOf(owner.address)).to.equal(1);
+            const mintAnotherPoe = await poeNft.mintToken(owner.address, "https://your-metadata-api.herokuapp.com/api/token/");
+            await mintAnotherPoe.wait();            
+
+            expect(await poeNft.balanceOf(owner.address)).to.equal(2);
+
+            expect(await poeNft.balanceOf(addr1.address)).to.equal(0);
+
+            const transferToken = await poeNft.transferFrom(owner.address, addr1.address, 2);
+            await transferToken.wait();
+
+            expect(await poeNft.balanceOf(addr1.address)).to.equal(1);
+
+            expect(await poeNft.ownerOf(2)).to.equal(addr1.address);
+            expect(await poeNft.ownerOf(1)).to.equal(owner.address);
+        });
     });
 });
