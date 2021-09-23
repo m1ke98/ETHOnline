befaaa3ad595c577ddedd4e48934806ebeb6da16
Harry G. Dulaney  IV <harrydulaney@gmail.com>
2021-09-23 08:19:19 -0400

    Added infura api key to constants

--- eth-app/packages/react-app/src/constants.js
+++ eth-app/packages/react-app/src/constants.js
@@ -1,2 +1,6 @@
-export const NFT_STORAGE_ENDPOINT="https://api.nft.storage"
-export const NFT_STORAGE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDUwMTgyNmY5YmQ4YTgzOTU3RkE3OEE0QTkwMjk0N2E2NGJmZTQyNTkiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYzMjE4NzIyNzQ2NCwibmFtZSI6IlNvbWV0aGluZ0Nvb2xBcGlLZXkifQ.IAWS_6tZMKUhOaSRjKtr20Pjw4vO6gsGjn2Sl59jAGk"
+export const INFURA_API_URI_RINKEBY= "https://rinkeby.infura.io/v3";
+export const INFURA_API_URI_ROPSTEN = "https://ropsten.infura.io/v3/";
+export const INFURA_API_URI_MAINNET = "https://mainnet.infura.io/v3/";
+export const INFURA_API_KEY = "b58725ea45f647beac39eee4a2b379f0";
+export const NFT_STORAGE_ENDPOINT = "https://api.nft.storage";
+export const NFT_STORAGE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDUwMTgyNmY5YmQ4YTgzOTU3RkE3OEE0QTkwMjk0N2E2NGJmZTQyNTkiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYzMjE4NzIyNzQ2NCwibmFtZSI6IlNvbWV0aGluZ0Nvb2xBcGlLZXkifQ.IAWS_6tZMKUhOaSRjKtr20Pjw4vO6gsGjn2Sl59jAGk";
