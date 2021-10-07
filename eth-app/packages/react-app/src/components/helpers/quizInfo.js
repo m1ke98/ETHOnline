cfad2aa3dc7a360e09524b2b37dcc83cfd998ad9
Michael D'Argenio <mdargenio14@gmail.com>
2021-10-06 20:42:57 -0400

    Quiz done

--- eth-app/packages/react-app/src/components/helpers/quizInfo.js
+++ eth-app/packages/react-app/src/components/helpers/quizInfo.js
@@ -1,41 +1,44 @@
 export const questions = [
     {
-      number: 1,
-      question: 'Which project is not considered a DeFi application?',
-      a1: 'Uniswap',
-      a2: '1inch',
-      a3: 'OpenSea',
-      a4: 'Compound',
-    },
-    {
-      number: 2,
-      question: 'Who developed Bitcoin?',
-      a1: 'Satoshi Nakamoto',
-      a2: 'Bill Gates',
-      a3: 'Elon Musk',
-      a4: 'Vitalik Buterin',
-    },
-    {
-      number: 3,
-      question: 'Which project created the first NFTs on Ethereum?',
-      a1: 'CryptoKitties',
-      a2: 'Axie Infinity',
-      a3: 'Decentraland',
-      a4: 'CryptoPunks',
-    },
-];
-
-export const answers = [
-    {
         number: 1,
-        answer: 'a3', 
+        question: 'Which project is not considered a DeFi application?',
+        a1: 'Uniswap',
+        a2: '1inch',
+        a3: 'OpenSea',
+        a4: 'Compound',
     },
     {
         number: 2,
-        answer: 'a1', 
+        question: 'Who developed Bitcoin?',
+        a1: 'Satoshi Nakamoto',
+        a2: 'Bill Gates',
+        a3: 'Elon Musk',
+        a4: 'Vitalik Buterin',
     },
     {
         number: 3,
-        answer: 'a4',
+        question: 'Which project created the first NFTs on Ethereum?',
+        a1: 'CryptoKitties',
+        a2: 'Axie Infinity',
+        a3: 'Decentraland',
+        a4: 'CryptoPunks',
     },
-]
+    {
+        number: 4,
+        question: 'What is the total number of Bitcoins that can exist?',
+        a1: '1,000,000',
+        a2: '21,000,000',
+        a3: '21,000',
+        a4: 'Unlimited',
+    },
+    {
+        number: 5,
+        question: 'What is the max amount of Ethereum that can ever exist?',
+        a1: 'Unlimited',
+        a2: '21,000,000',
+        a3: '10,000,000',
+        a4: '100,000,000',
+    },
+];
+
+export const quizAnswers = ['a3', 'a1', 'a4', 'a2', 'a1'];
