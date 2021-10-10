4749de52d2009ebdb7c39dbec279ce8a0dcf57e9
Michael D'Argenio <mdargenio14@gmail.com>
2021-10-09 23:24:09 -0400

    cleanup

--- eth-app/packages/react-app/src/components/Quiz.js
+++ eth-app/packages/react-app/src/components/Quiz.js
@@ -29,8 +29,6 @@ export default function Quiz() {
     const [answers, setAnswers] = useState(new Array(questions.length));
     const [score, setScore] = useState(0);
 
-    // UseMemo(); to fix the answer update status.
-
     useEffect(() => {
         generateScore();
     });
