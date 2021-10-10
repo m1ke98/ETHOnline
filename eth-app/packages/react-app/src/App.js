174f3016f32a790954a8a5869a0341eec401ac18
Michael D'Argenio <mdargenio14@gmail.com>
2021-10-10 17:44:29 -0400

    Hide test

--- eth-app/packages/react-app/src/App.js
+++ eth-app/packages/react-app/src/App.js
@@ -1,38 +1,16 @@
 import React, { useEffect, useState } from "react";
 import useWeb3Modal from "./hooks/useWeb3Modal";
-// import { useQuery } from "@apollo/react-hooks";
-//import { Contract } from "@ethersproject/contracts";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { getTxHistoryByAddress } from "./components/helpers/http-client";
 import { addresses } from "@project/contracts";
 
 
-//import { getDefaultProvider } from "@ethersproject/providers";
-
-// For readOnChainData() function
-//import { addresses, abis } from "@project/contracts";
-
-// import GET_TRANSFERS from "./graphql/subgraph";
-
 import Home from "./components/Home.js";
 import Navbar from "./components/Navbar.js";
 import Profile from "./components/Profile.js";
 import Mint from "./components/Mint.js";
-import Test from "./components/Test.js";
 import Quiz from "./components/Quiz.js";
-
-/*
-async function readOnChainData() {
-  // Should replace with the end-user wallet, e.g. Metamask
-  const defaultProvider = getDefaultProvider();
-  // Create an instance of an ethers.js Contract
-  // Read more about ethers.js on https://docs.ethers.io/v5/api/contract/contract/
-  const ceaErc20 = new Contract(addresses.ceaErc20, abis.erc20, defaultProvider);
-  // A pre-defined address that owns some CEAERC20 tokens
-  const tokenBalance = await ceaErc20.balanceOf("0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C");
-  console.log({ tokenBalance: tokenBalance.toString() });
-}
-*/
+// import Test from "./components/Test.js";
 
 function App() {
 
@@ -96,13 +74,6 @@ function App() {
     fetchTransactions();
   }, [account, provider]);
 
-  // const { loading, error, data } = useQuery(GET_TRANSFERS); // For use with GraphQL
-  // For GraphQl
-  // React.useEffect(() => {
-  //   if (!loading && !error && data && data.transfers) {
-  //     console.log({ transfers: data.transfers });
-  //   }
-  // }, [loading, error, data]);
 
   function web3Modal() {
     if (!provider) {
@@ -127,9 +98,9 @@ function App() {
           <Route path="/mint">
             <Mint web3Modal={web3Modal} provider={provider} account={account} />
           </Route>
-          <Route path="/test">
+          {/* <Route path="/test">
             <Test account={account} provider={provider} />
-          </Route>
+          </Route> */}
           <Route path="/quiz">
             <Quiz account={account} provider={provider} />
           </Route>
